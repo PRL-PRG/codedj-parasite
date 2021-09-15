37163492b706dd3f570a36ed03541889305ec5f0
peta <peta.maj82@gmail.com>
2021-09-15 14:28:56 +0200

    clap added to parasite args.

--- src/parasite.rs
+++ src/parasite.rs
@@ -1,3 +1,24 @@
+#[macro_use]
+extern crate clap;
+use clap::{App, ArgMatches};
+
 fn main() {
-    println!("Hello, world!");
+    let yaml = load_yaml!("parasite-cli.yaml");
+    let matches = App::from_yaml(yaml).get_matches();
+
+    match matches.subcommand() {
+        ("create", Some(args)) => create(& matches, args),
+        ("add", Some(args)) => add(& matches, args),
+        _ => {
+            panic!("Invalid command-line arguments");
+        }
+    }
 }
+
+fn create(_args : & ArgMatches, _cmd_args : & ArgMatches) {
+
+} 
+
+fn add(_args : & ArgMatches, _cmd_args : & ArgMatches) {
+    
+} 
